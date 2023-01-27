1c3d9f0429eb1e3761d7ea8c8aaa22fe16d76593
SebDors <dorssebastien@gmail.com>
2023-01-27 13:43:50 +0100

    Feat to add : boucle

--- Main.py
+++ Main.py
@@ -35,50 +35,59 @@ def Distance_Villes(VilleA, VilleB):
     return Rayon*(np.arccos(np.sin(LatA)*np.sin(LatB)+np.cos(LatA)*np.cos(LatB)*np.cos(LongB-LongA)))
 
 
+# Comparer la distance entre l'actuelle et la minimale
+def ComparaisonDistance(x):
+    if LongueurMin > x:
+        LongueurMin = LongueurActuelle
+        TrajetMin = TrajetActuel
+
+
 LongueurMin = 0
 TrajetMin = ""
-#NmbIterations = int(input("Number of iterations ?"))
+NmbIterations = int(input("Number of iterations ?"))
 VilleDépart = "Paris"
 
-"""for i in range(NmbIterations):"""
-LongueurActuelle = 0
-TrajetActuel = ""
-while Liste_Des_Villes[3][0] == 0:
-    # Choix de la prochaine destination
-    ChoixDeVille = np.random.randint(1, (len(Liste_Des_Villes[0])))
-
-    while Liste_Des_Villes[3][ChoixDeVille] == 1:
-        # Check si nous n'avons pas pris une destination déjà prise
+for i in range(NmbIterations):
+    LongueurActuelle = 0
+    TrajetActuel = VilleDépart
+    while Liste_Des_Villes[3][0] == 0:
+        # Choix de la prochaine destination
         ChoixDeVille = np.random.randint(1, (len(Liste_Des_Villes[0])))
-    VilleEtape = Liste_Des_Villes[0][ChoixDeVille]
 
-    # Ajouter un 1 pour prevenir que nous sommes déjà passé par cette destination
-    Liste_Des_Villes[3][ChoixDeVille] = 1
-    # Donner la destination avec le kilometrage
-    print(VilleDépart, "-> ", VilleEtape, " = ", Distance_Villes(VilleDépart, VilleEtape)/1000, "km")
+        while Liste_Des_Villes[3][ChoixDeVille] == 1:
+            # Check si nous n'avons pas pris une destination déjà prise
+            ChoixDeVille = np.random.randint(1, (len(Liste_Des_Villes[0])))
+        VilleEtape = Liste_Des_Villes[0][ChoixDeVille]
 
-    # Mettre à jour la distance actuelle
-    LongueurActuelle += Distance_Villes(VilleDépart, VilleEtape)/1000
+        # Ajouter un 1 pour prevenir que nous sommes déjà passé par cette destination
+        Liste_Des_Villes[3][ChoixDeVille] = 1
+        # Donner la destination avec le kilometrage
+        """print(VilleDépart, "-> ", VilleEtape, " = ", Distance_Villes(VilleDépart, VilleEtape)/1000, "km")"""
+        # Mettre à jour la distance actuelle
+        LongueurActuelle += Distance_Villes(VilleDépart, VilleEtape)/1000
 
-    # Intervertir Ville de départ et ville d'arrivée
-    VilleDépart = VilleEtape
+        # Integrer les étapes dans notre Trajet actuelle
+        TrajetActuel += " -> " + VilleEtape
+        # Intervertir Ville de départ et ville d'arrivée
+        VilleDépart = VilleEtape
 
-    # Checker si nous n'avons pas fais toutes les destinations
-    # Additionner la dernière ligne de toutes les villes pour savoir si nous sommes passé par toutes proposées
-    count = 0
-    for i in range(len(Liste_Des_Villes[3])):
-        x = Liste_Des_Villes[3][i]
-        count += x
-        # print(count)
-    # Comparer le nombres de villes traversés avec le nombre total possible
-    if count == (len(Liste_Des_Villes[3])-1):
-        # Passer de la dernière ville à Paris (Ville de fin)
-        LongueurActuelle += Distance_Villes(VilleDépart, "Paris")/1000
-        print(VilleDépart, "-> Paris = ", Distance_Villes(VilleDépart, "Paris")/1000, "km")
-        # Spécifier que nous sommes passé par Paris
-        Liste_Des_Villes[3][0] = 1
-        print("here")
-        break
+        # Checker si nous n'avons pas fais toutes les destinations
+        # Additionner la dernière ligne de toutes les villes pour savoir si nous sommes passé par toutes proposées
+        count = 0
+        for i in range(len(Liste_Des_Villes[3])):
+            x = Liste_Des_Villes[3][i]
+            count += x
+            # print(count)
+        # Comparer le nombres de villes traversés avec le nombre total possible
+        if count == (len(Liste_Des_Villes[3])-1):
+            # Passer de la dernière ville à Paris (Ville de fin)
+            LongueurActuelle += Distance_Villes(VilleDépart, "Paris")/1000
+            """print(VilleDépart, "-> Paris = ", Distance_Villes(VilleDépart, "Paris")/1000, "km")"""
+            # Spécifier que nous sommes passé par Paris
+            Liste_Des_Villes[3][0] = 1
+            break
 
-print("finish")
-print("La longueur est :", round(LongueurActuelle), "km")
+    print("finish")
+    print("La longueur est :", round(LongueurActuelle), "km")
+    print("le trajet est : ", TrajetActuel)
+    ComparaisonDistance(round(LongueurActuelle))
